75a080dd008d2e9cd30575ce3535b3a51c629218
WoodenBrick1 <brickwooden@gmail.com>
2025-06-25 18:08:43 +0200

    Made the api displayed

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,4 +1,30 @@
+import {useState, useEffect} from "react"
+import "../styles/cards.css"
 
 export default function Cards() {
-    return <h3>Cards</h3>
+    const [cardsData, setCardsData] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.disneyapi.dev/character?tvShows=The%20Owl%20House")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            setCardsData(data.data)
+        })
+        .catch(() => {
+            console.log("ERROR")
+        })
+    }, [])
+
+    const cards = cardsData.map(card => {
+        return (
+        <div className="card" key={card.name}>
+            <img src={card.imageUrl}></img>
+            <p className="cardName">{card.name}</p>
+        </div>)
+    })
+    return (
+    <div id="class-container"> 
+       {cards}
+    </div>)
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ import "../styles/header.css"
 export default function Header() {
     return (
     <header>
-        <h1 className="title">Adventure Cards</h1>
+        <h1 className="title">The Owl Cards</h1>
         <div className="score">
             <p>Score: </p>
             <p>High Score: </p>
